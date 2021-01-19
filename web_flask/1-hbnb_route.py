003f4a7261ea804a44d5b35bf8c5a7d8817b00cd
ZoltanMG <zoltanmoragarcia@gmail.com>
2021-01-19 01:52:25 +0000

    [add]: flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-script that starts a Flask web application
+script that starts a Flask web application:
 """
 
 from flask import Flask
@@ -15,6 +15,13 @@ def index():
     """
     return "Hello HBNB!"
 
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    route /hbnb
+    """
+    return "HBNB"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
